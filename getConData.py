7480c14a7354e3e25e6fcc4e1eff574ea2d0a66b
Joshua Boyd <jaboydfamily@gmail.com>
2021-06-20 10:11:05 -0400

    changed request type to GET

--- getConData.py
+++ getConData.py
@@ -3,7 +3,7 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET'])
 def index():
     visitorIP = flask.request.remote_addr
     return "Visitor IP: " + visitorIP;
