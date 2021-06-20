780dc88fe6d272fa3e0d11d4d183a95cbdf4b3b9
Joshua Boyd <jaboydfamily@gmail.com>
2021-06-20 10:46:50 -0400

    changed line that gets visitor ip

--- getConData.py
+++ getConData.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    visitorIP = request.environ['REMOTE_ADDR']
+    visitorIP = request.headers.getlist("X-Forwarded-For")[0]
     return "Visitor IP: " + visitorIP;
 
 if __name__ == "__main__":
