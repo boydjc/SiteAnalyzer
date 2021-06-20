6778c2a1cf176caaf4401aa93cd14dd5fee11add
Joshua Boyd <jaboydfamily@gmail.com>
2021-06-20 12:44:57 -0400

    added database functionality

--- getConData.py
+++ getConData.py
@@ -1,17 +1,24 @@
 from flask import Flask, request
-
+from datetime import datetime
+import sqlite3
 
 app = Flask(__name__)
+con = sqlite3.connect('conData.db')
 
 @app.route('/', methods=['GET'])
 def index():
+
+    cur.execute('''CREATE TABLE connections (date text, ip text)''')
+
     potentIPs = request.headers.getlist("X-Forwarded-For")[0]
 
     potentIPList = potentIPs.split(',')
 
     visitorIP = potentIPList[len(potentIPList)-1]
 
-    return "Visitor IP: " + visitorIP;
+    cur.execute('INSERT INTO connections ("' + datetime.date + '", "' + visitorIP + ')')
+
+    return "Visitor IP stored"
 
 if __name__ == "__main__":
     app.run();
