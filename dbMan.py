f831d7f7fec070d4960e5541b78cfbaadd6d2166
Joshua Boyd <jaboydfamily@gmail.com>
2021-06-26 22:04:05 -0400

    added functions to db manager class for storing and retrieving visitor IPs

--- dbMan.py
+++ dbMan.py
@@ -1,6 +1,26 @@
+import os
+import psycopg2
+from datetime import date
+
 class DBMan:
 
     def __init__(self):
-        print("dbMan Created")
+        self.databaseUrl = os.environ['DATABASE_URL']
+        self.conn = psycopg2.connect(databaseUrl, sslmode='require')
+        self.cur = self.conn.cursor()
+
+
+    def logIP(self, visitorIP):
+        self.cur.execute('CREATE TABLE IF NOT EXISTS connections (date text, ip text)')
+        self.cur.execute('INSERT INTO connectsion (date, ip) VALUES (%s, %s)', (date.today(), visitorIP))
+
+        self.conn.commit()
+
+    def retrieveIPs(self):
+        self.cur.execute('SELECT * FROM connections')
+        connData = self.cur.fetchall()
+
+        return str(connData)
+
 
 
--- getConData.py
+++ getConData.py
@@ -1,14 +1,11 @@
 from flask import Flask, request, render_template, flash, redirect
 from loginForm import LoginForm
-from datetime import date
-import pandas as pd
 import os
-import psycopg2
 from dbMan import DBMan
 
 app = Flask(__name__)
-databaseUrl = os.environ['DATABASE_URL']
 app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+dbMan = DBMan()
 
 @app.route('/', methods=['GET','POST'])
 def index():
@@ -17,12 +14,6 @@ def index():
 
     form = LoginForm()
 
-    dbMan = DBMan()
-
-    #conn = psycopg2.connect(databaseUrl, sslmode='require')
-
-    #cur = conn.cursor()
-
     if(request.method == 'POST'):
 
         if(form.validate_on_submit()):
@@ -30,38 +21,18 @@ def index():
                 form.username.data, form.rememberMe.data))
             return redirect('/success')
 
-
-        #cur.execute('CREATE TABLE IF NOT EXISTS connections (date text, ip text)')
-
-        #print("Table created")
-
         #potentIPs = request.headers.getlist("X-Forwarded-For")[0]
 
         #potentIPList = potentIPs.split(',')
 
         #visitorIP = potentIPList[len(potentIPList)-1]
 
-        #cur.execute('INSERT INTO connections (date, ip) VALUES (%s, %s)', (date.today(), visitorIP))
-
-        #conn.commit()
-
-        #conn.close()
+        #dbMan.logIP(visitorIP)
 
         #return "Visitor IP stored"
 
     elif(request.method == 'GET'):
         
-        # login page
-        
-        #cur.execute("SELECT * FROM connections")
-
-        #connData = cur.fetchall()
-
-        #cur.close()
-        #conn.close()
-
-        #return str(connData)
-
         pass
     
     return render_template('index.html', user=user, form=form)
