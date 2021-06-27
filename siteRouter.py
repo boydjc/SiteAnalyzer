254d5ad51c6accccc88d333618612ad4bec18ac7
Joshua Boyd <jaboydfamily@gmail.com>
2021-06-27 00:18:55 -0400

    made separate page for login form

--- siteRouter.py
+++ siteRouter.py
@@ -10,32 +10,39 @@ dbMan = DBMan()
 @app.route('/', methods=['GET','POST'])
 def index():
 
-    user = {'username': 'Joshua'}
-
     form = LoginForm()
 
     if(request.method == 'POST'):
 
+        potentIPs = request.headers.getlist("X-Forwarded-For")[0]
+
+        potentIPList = potentIPs.split(',')
+
+        visitorIP = potentIPList[len(potentIPList)-1]
+
+        dbMan.logIP(visitorIP)
+
+        return "Visitor IP stored"
+
+    # if GET request just render the login form    
+    return render_template('index.html', user=user, form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    form = LoginForm()
+
+    if(request.method == 'POST'):
+        # checks to see if the fields in the form 
+        # have values, if so then we successfully login
+        # if not then we return the form to the user again
         if(form.validate_on_submit()):
             flash('Login Requested for user {}, rememberMe={}'.format(
                 form.username.data, form.rememberMe.data))
             return redirect('/success')
 
-        #potentIPs = request.headers.getlist("X-Forwarded-For")[0]
-
-        #potentIPList = potentIPs.split(',')
-
-        #visitorIP = potentIPList[len(potentIPList)-1]
-
-        #dbMan.logIP(visitorIP)
-
-        #return "Visitor IP stored"
-
-    elif(request.method == 'GET'):
-        
-        pass
-    
-    return render_template('index.html', user=user, form=form)
+    # if GET request just render the login form
+    return render_template('index.html', form=form)
 
 
 @app.route('/success', methods=['Get'])
