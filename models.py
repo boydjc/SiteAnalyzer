6190fd6cb377cbfc9b984b5c2939ebdd4dddc838
Joshua Boyd <jaboydfamily@gmail.com>
2021-06-30 12:41:57 -0400

    changed user table in database to accounts

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from siteRouter import db
 from werkzeug.security import generate_password_hash, check_password_hash
 
-class User(db.Model):
+class Accounts(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
