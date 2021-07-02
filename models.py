e902f93ba7ca5f041552418d8c8c674cb1886795
Joshua Boyd <jaboydfamily@gmail.com>
2021-07-02 00:09:06 -0400

    fixed import error

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-from app import login
+from siteRouter import login
 
 # assists Flask-Login with loading the account
 @login.user_loader
