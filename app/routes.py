190e3a2db6b53b1683f6700021de1e7e5c98c64b
Joshua Boyd <jaboydfamily@gmail.com>
2021-07-04 00:13:28 -0400

    added login_required module to router

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template, flash, redirect
 from werkzeug.urls import url_parse
 from app.forms import LoginForm, LogoutForm
-from flask_login import current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user, login_required
 from app.models import Account
 import os
 from datetime import date
