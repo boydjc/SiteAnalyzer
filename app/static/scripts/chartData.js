19f84cfbf2a7246ff8aa1126e08bae90caec9f3b
Joshua Boyd <jaboydfamily@gmail.com>
2021-07-06 11:57:18 -0400

    merges homeBase and home.html

--- app/static/scripts/chartData.js
+++ app/static/scripts/chartData.js
@@ -2,8 +2,7 @@ document.addEventListener("DOMContentLoaded", function(){
     this.getElementById("refreshButton").addEventListener("click", function(){
         var xhttp = new XMLHttpRequest();
 	xhttp.onload = function () {
-            connectionData = this.responseText;
-	    console.log(connectionData);
+            var connectionData = this.responseText;
 	}
 	xhttp.open("GET", "/data", true);
 	xhttp.send();
