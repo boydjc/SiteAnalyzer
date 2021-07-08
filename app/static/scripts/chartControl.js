fc160b1b09d5c4bfc334155c64e91f6c794c18c6
Joshua Boyd <jaboydfamily@gmail.com>
2021-07-08 12:49:08 -0400

    added time series parameter to connection chart function

--- app/static/scripts/chartControl.js
+++ app/static/scripts/chartControl.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function(){
             var connectionData = JSON.parse(this.responseText);
 	    if(Chart.getChart('myChart')){
                 Chart.getChart('myChart').destroy();
-		drawChart(connectionData);
+		drawConnChart(connectionData, "month");
 	    }else{
 	        drawChart(connectionData);
 	    }
@@ -20,114 +20,91 @@ document.addEventListener("DOMContentLoaded", function(){
 	xhttp.send();
     }
 
-    // takes a JSON object of connection data and counts the 
-    // dates of occurance
-    // TODO: make 2nd parameter to count either months, days or years
-    function getDateCount(connCountDataInput) {
-
-	connCountData = {};
-
-	var connDataKeys = Object.keys(connCountDataInput);
-
-        for(key in connDataKeys){
-	    if(connCountDataInput[connDataKeys[key]].dateVisited in connCountData){
-	        connCountData[connCountDataInput[connDataKeys[key]].dateVisited] += 1;
-	    }else{
-                connCountData[connCountDataInput[connDataKeys[key]].dateVisited] = 1;
-	    }
-	}
-
-	return connCountData;
-    }
-
     // draws chart for connections, 
     // TODO: make 2nd parameter to draw different time series
-    function drawChart(connDataInput) {
+    function drawConnChart(connDataInput, timeSeries) {
 
 	var connDataKeys = Object.keys(connDataInput);
 
-	var janCount = 0; var febCount = 0; var marCount = 0;
-	var aprCount = 0; var mayCount = 0; var junCount = 0;
-	var julCount = 0; var augCount = 0; var sepCount = 0;
-	var octCount = 0; var novCount = 0; var decCount = 0;
+	if(timeSeries === "month"){
 
-	// counts the occurance of connections per month
-	for(key in connDataKeys){
-	    if(connDataInput[connDataKeys[key]].dateVisited){
+	    var janCount = 0; var febCount = 0; var marCount = 0;
+	    var aprCount = 0; var mayCount = 0; var junCount = 0;
+	    var julCount = 0; var augCount = 0; var sepCount = 0;
+	    var octCount = 0; var novCount = 0; var decCount = 0;
 
-	        visitedMonth = connDataInput[connDataKeys[key]].dateVisited[5] + 
-			    connDataInput[connDataKeys[key]].dateVisited[6];
+	    // counts the occurance of connections per month
+	    for(key in connDataKeys){
+	        if(connDataInput[connDataKeys[key]].dateVisited){
 
-		switch(visitedMonth){
-		    case "01":
-			janCount += 1;
-			break;
-		    case "02":
-			febCount += 1;
-			break;
-		    case "03":
-			marCount += 1;
-			break;
-		    case "04":
-			aprCount += 1;
-			break;
-		    case "05":
-			mayCount += 1;
-			break;
-		    case "06":
-			junCount += 1;
-			break;
-		    case "07":
-		 	julCount += 1;
-			break;
-		    case "08":
-			augCount += 1;
-			break;
-		    case "09":
-			sepCount += 1;
-			break;
-		    case "10":
-			octCount += 1;
-			break;
-		    case "11":
-			novCount += 1;
-			break;
-		    case "12":
-			decCount += 1;
-			break;
-		    default:
-			break;
-		}
+	            visitedMonth = connDataInput[connDataKeys[key]].dateVisited[5] + 
+			        connDataInput[connDataKeys[key]].dateVisited[6];
+
+		    switch(visitedMonth){
+		        case "01":
+			    janCount += 1;
+			    break;
+		        case "02":
+			    febCount += 1;
+			    break;
+		        case "03":
+			    marCount += 1;
+			    break;
+		        case "04":
+			    aprCount += 1;
+		 	    break;
+		        case "05":
+			    mayCount += 1;
+			    break;
+		        case "06":
+			    junCount += 1;
+			    break;
+		        case "07":
+		 	    julCount += 1;
+		 	    break;
+		        case "08":
+			    augCount += 1;
+			    break;
+		        case "09":
+			    sepCount += 1;
+			    break;
+		        case "10":
+			    octCount += 1;
+			    break;
+		        case "11":
+			    novCount += 1;
+			    break;
+		        case "12":
+			    decCount += 1;
+			    break;
+		        default:
+			    break;
+		    }
+	        }
 	    }
+	
+	    const labels = [
+	        'January','February','March','April',
+		'May','June','July','August',
+	        'September','October','November','December',
+	    ];
+	    
+	    // TODO: clean up the code redundance of this data obj
+	    // for when we have multiple time series
+	    // maybe some sort of object to hold the data values
+	    const data = {
+                labels: labels,
+	        datasets: [{
+	            label: 'Connections',
+		    backgroundColor: 'rgb(0,0,0)',
+		    borderColor: 'rgb(0,0,0)',
+		    data: [janCount, febCount, marCount, 
+		           aprCount, mayCount, junCount,
+		           julCount, augCount, sepCount,
+		           octCount, novCount, decCount],
+	        }]
+	    };
 	}
-
-	const labels = [
-	    'January',
-	    'February',
-	    'March',
-	    'April',
-	    'May',
-	    'June',
-	    'July',
-	    'August',
-	    'September',
-	    'October',
-	    'November',
-	    'December',
-	];
-
-	const data = {
-            labels: labels,
-	    datasets: [{
-	        label: 'Connections',
-		backgroundColor: 'rgb(0,0,0)',
-		borderColor: 'rgb(0,0,0)',
-		data: [janCount, febCount, marCount, 
-		       aprCount, mayCount, junCount,
-		       julCount, augCount, sepCount,
-		       octCount, novCount, decCount],
-	    }]
-	};
 
         const config = {
             type: 'line',
