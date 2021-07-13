b8b302bf85424e87937f9c9780e36bc21b67184a
Joshua Boyd <jaboydfamily@gmail.com>
2021-07-12 21:38:19 -0400

    added class for custom bubble chart

--- app/static/scripts/chartControl.js
+++ app/static/scripts/chartControl.js
@@ -1,36 +1,33 @@
-class Custom extends Chart.BubbleController {
-    draw() {
+document.addEventListener("DOMContentLoaded", function(){
+
+    class Custom extends Chart.BubbleController {
+        draw() {
+	 
+	    // Call bubble controller method to draw all the points
+            super.draw(arguments);
 	
-	// Call bubble controller method to draw all the points
-        super.draw(arguments);
-	
-	const meta = this.getMeta();
-	const pt0 = meta.data[0];
+	    const meta = this.getMeta();
+	    const pt0 = meta.data[0];
 
-	const {x, y} = pt0.getProps(['x', 'y']);
-	const {radius} = pt0.options;
+	    const {x, y} = pt0.getProps(['x', 'y']);
+	    const {radius} = pt0.options;
 
-	const ctx = this.chart.ctx;
+	    const ctx = this.chart.ctx;
 
-	ctx.save();
+	    ctx.save();
 
-	ctx.strokeStyle = 'red';
-	ctx.lineWidth = 1;
-	ctx.strokeRect(x - radius, y - radius, 2 * radius, 2 * radius);
-	ctx.restore();
+	    ctx.strokeStyle = 'red';
+	    ctx.lineWidth = 1;
+	    ctx.strokeRect(x - radius, y - radius, 2 * radius, 2 * radius);
+	    ctx.restore();
+        }
     }
-}
 
 Custom.id = 'derivedBubble';
 Custom.defaults = BubbleController.defaults;
 
 // Stores the controller so that the chart initialization routine can look it up
 Chart.register(Custom);
-
-
-
-document.addEventListener("DOMContentLoaded", function(){
-
 
     var connectionData;
 
